c5bc92a0f04f865c5ed40e70100cb99b8e4f34a6
Leo Rubiano <leorubiano@Leos-MacBook-Pro.local>
2017-09-12 15:46:49 -0400

    more work on week 1

--- js/script.js
+++ js/script.js
@@ -1,8 +1,9 @@
 $('document').ready(function(){
   $('#j-highlights-row').hide();
+  $('#j-commentary-row').hide();
 });
 
-
+//Highlights display
 $('.highlights-header').on('click', function(){
   if($('.highlights-header').hasClass('displaying')){
     $('.highlights-header').removeClass('displaying');
@@ -11,5 +12,18 @@ $('.highlights-header').on('click', function(){
   else {
     $('.highlights-header').addClass('displaying');
     $('#j-highlights-row').show();
+  }
+});
+
+
+//Commentary Display
+$('.commentary-header').on('click', function(){
+  if($('.commentary-header').hasClass('displaying')){
+    $('.commentary-header').removeClass('displaying');
+    $('#j-commentary-row').hide();
+  }
+  else {
+    $('.commentary-header').addClass('displaying');
+    $('#j-commentary-row').show();
   }
 });
